4c88956a0a47efefe2a0a62fa20f9ed4791b1dda
rikimario <mario_martinov@abv.bg>
2023-10-29 00:02:33 +0300

    change

--- src/components/AddButton.jsx
+++
@@ -1,7 +0,0 @@
-export default function AddButton() {
-    return (
-        <div className="add-btn-container">
-            <button className="btn">+ Add new Todo</button>
-        </div>
-    )
-};
